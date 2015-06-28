906f722d3e5f196e5d74787af7e16529287b17f5
Josh Eklund <joshe@hranswerlink.com>
2015-06-28 13:46:16 -0700

    the self-portrait almost looks portrait like

--- solutions/chapter-1/self-portrait.elm
+++ solutions/chapter-1/self-portrait.elm
@@ -10,13 +10,46 @@ head : Element
 head =
   collage 300 300
     [ skull
-    , circle 50
-        |> filled green
-        |> move (50,10)
+    , leftEye
+    , rightEye
+    , leftPupil
+    , rightPupil
     ]
   
 skull : Form
 skull =
   circle 75
-    |> filled green
+    |> filled skull_color
     |> move (-10,0)
+
+leftEye : Form
+leftEye =
+  circle 10
+    |> filled eyeColor
+    |> move (50, 10)
+
+leftPupil : Form
+leftPupil =
+  circle 5
+    |> filled black
+    |> move (50, 10)
+
+rightEye : Form
+rightEye =
+  circle 10
+    |> filled eyeColor
+    |> move (10, 10)
+
+rightPupil : Form
+rightPupil =
+  circle 5
+    |> filled black
+    |> move (10, 10)
+
+skull_color : Color
+skull_color =
+  rgb 75 130 0
+
+eyeColor : Color
+eyeColor =
+  rgb 0 168 107
