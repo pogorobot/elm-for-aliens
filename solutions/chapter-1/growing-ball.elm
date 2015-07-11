e8905cf5d553dc8b5122bec692b590aa08826db4
Josh Eklund <joshe@hranswerlink.com>
2015-07-11 01:32:57 -0700

    growing ball actually grows. procedural animation achieved!

--- solutions/chapter-1/growing-ball.elm
+++ solutions/chapter-1/growing-ball.elm
@@ -1,16 +1,21 @@
 import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
-import StartApp
 import Html exposing (..)
+import Time exposing (..)
+import Signal exposing (..)
 
 -- MODEL
 
 type alias Model = Int
 
+init : Model
+init = 0
+
 -- UPDATE
 
 type Action = Tick | NoOp
+
 
 update : Action -> Model -> Model
 update action model =
@@ -18,17 +23,40 @@ update action model =
     Tick -> model + 1
     NoOp -> model
 
+clock : Signal Time
+clock =
+  Time.fps 30
+
+-- This is the mailbox that receives all the actions
+-- that will update the application.
+actionMailbox : Mailbox Action
+actionMailbox =
+  Signal.mailbox NoOp
+
+address : Address Action
+address =
+  actionMailbox.address
+
+actions : Signal Action
+actions =
+  Signal.map ticker clock
+
+ticker : Time -> Action
+ticker time =
+  Tick
+
 -- VIEW
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   collage 300 300
   [
-    cos (toFloat model) + 50
+    cos (degrees (toFloat model)) * 50
       |> circle
       |> filled green
   ]
    |> fromElement
 
-main = 
-  StartApp.start { model = 0, update = update, view = view }
+main =
+  Signal.map (view address)
+    (Signal.foldp update init actions) 
