c713b30acb12ed9177a7ec4a60445c0d6c8eafac
Josh Eklund <joshe@hranswerlink.com>
2015-07-25 00:05:06 -0700

    Change the signals so the ball is at war with itself.

--- solutions/chapter-1/growing-ball.elm
+++ solutions/chapter-1/growing-ball.elm
@@ -14,18 +14,25 @@ init = 0
 
 -- UPDATE
 
-type Action = Tick | NoOp
+type Action = Tick 
+  | Tock
+  | NoOp
 
 
 update : Action -> Model -> Model
 update action model =
   case action of 
-    Tick -> model + 1
+    Tick -> model + 4
+    Tock -> model - 1
     NoOp -> model
 
 clock : Signal Time
 clock =
   Time.fps 30
+
+cluck : Signal Time
+cluck =
+  Time.fps 60
 
 -- This is the mailbox that receives all the actions
 -- that will update the application.
@@ -39,11 +46,18 @@ address =
 
 actions : Signal Action
 actions =
-  Signal.map ticker clock
+  Signal.merge 
+    (Signal.map ticker clock)
+    (Signal.map clicker cluck)
+
 
 ticker : Time -> Action
 ticker time =
-  Tick
+  if floor time % 2 == 0 then Tick else Tock
+
+clicker : Time -> Action
+clicker time =
+  Tock
 
 -- VIEW
 
@@ -53,7 +67,7 @@ view address model =
   [
     cos (degrees (toFloat model)) * 50
       |> circle
-      |> filled green
+      |> filled (rgb 75 130 0)
   ]
    |> fromElement
 
